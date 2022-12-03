553ad2346cb3fc7884eaaa1f686b771fc0369885
Samuel Suesca Ríos <suescapsam@gmail.com>
2022-12-02 21:28:48 -0500

    Pagina ISLM

--- app.py
+++ app.py
@@ -1,49 +1,137 @@
 import streamlit as st
 import pandas as pd 
 import numpy as np
-from is_lm import ISLM
+import sympy as sp
+from is_lm import ISLM, ISLM_deploy, ISLM_get_description, definition
+
+M,P,k,h,c,t,b,Ca,Ta,Ia,Tr,G,NX = sp.symbols('M,P,k,h,c,t,b,Ca,Ta,Ia,Tr,G,NX')
 
 def main():
     st.set_page_config(page_title='Blog Economia',
                     page_icon=':notebook:',
                     layout='wide')
+
+    ##PRINCIPAL 
     st.title('Modelo IS-LM')
-    st.sidebar.header('Entrada de parametros modelo')
-
-    def user_input_parameters():
-        M = st.sidebar.slider('Oferta Monetaria',0,10000,400)
-        P = st.sidebar.slider('Nivel de Precios',0,10000,1)
-        k = st.sidebar.slider('Sensibilidad a la Renta',0,10000,4)
-        h = st.sidebar.slider('Sensibilidad al tipo de interes',0,10000,50)
-        c = st.sidebar.slider('Propensión al consumo',min_value=float(0), max_value=float(1),value=0.8,step=0.01)
-        t = st.sidebar.slider('Tasa Impositiva',min_value=float(0), max_value=float(1),value=0.2,step=0.01)
-        b = st.sidebar.slider('Sensibilidad de la inversion',0,10000,40)
-        Ca = st.sidebar.slider('Consumo Autonomo',0,10000,180)
-        Ta = st.sidebar.slider('Impuesto Autonomo',0,10000,50)
-        Ia = st.sidebar.slider('Interes Autonomo',0,10000,50)
-        Tr = st.sidebar.slider('Transferencias',0,10000,100)
-        G = st.sidebar.slider('Gasto',0,10000,500)
-        NX = st.sidebar.slider('Exportaciones Netas',0,10000,50)
-        data = {'Oferta Monetaria':round(M,2), 'Nivel de Precios':round(P,2), 'Sensibilidad a la Renta':round(k,2),
-                'Sensibilidad al tipo de interes':round(h,2), 'Pmg':round(c,2), 'Tasa Impositiva':round(t,2),
-                'Sensibilidad de la inversion':round(b,2), 'Consumo Autonomo':round(Ca,2),'Impuesto Autonomo':round(Ta,2),
-                'Interes Autonomo':round(Ia,2),'Trasnferencias':round(Tr,2),'Gasto':round(G,2),'Exportaciones Netas':round(NX,2)}
-        feactures = pd.DataFrame(data,index=['Parameters'])
-        return feactures
+    st.markdown("""---""")
+    leftcol1,rightcol1 = st.columns(2)
+    with leftcol1:
+        st.markdown('**GENERALIDADES**')
+        st.markdown('''Este sitio es un infograma interactivo sobre el modelo
+        IS-LM. Este modelo se trabaja en los primeros cursos de Macroeconomia, pues sirve como
+        introducción al análisis macro de una economía. En la siguiente imagen podemos ver una breve definición''')
+        st.image(definition())
+        st.markdown('''El modelo IS-LM (Investment-Saving and Liquidity Preference-Money Supply) parte del análisis de dos mercados, el de bienes y el de dinero, 
+        y termina por explicar las principales relaciones entre la política monetaria y la política fiscal. Este infograma, fue basado en el libro de 
+        Macroeconomía de Rudiger Dornbush, Stanley Fischer y Richard Startz, especialmente en los capitulos 10, 11 y 12 de la  12va edición
+        ''')
+        st.markdown('''En la parte derecha podrás encontrar la "sintexis" matemática del modelo,
+        para después, en la parte inferior, entrar a la parte interactiva, donde podrás entrar los parámetros de un modelo IS-LM estático. Y
+        obtener como resultado la gráfica del equilibrio de mercado, el planteamiento del modelo y  el procedimiento''')
+        st.markdown("""  """)
+        st.markdown("""  """)
+        st.download_button('Descarga el libro guía','book.pdf','Macroeconomia_DFS.pdf')
+        st.write('''Accede a esta lista de reproducción de youtube de la [Universidad de 
+        Valladolid](https://www.youtube.com/watch?v=BgbQB3jRxOI&list=PLSbo9kXA_Lcx5baMlEVo4s60RODjZiwsW) para acceder a un curso sobre IS-LM.''')
+        st.markdown("""  """)
+    with rightcol1:
+        results1 = ISLM_deploy(M,P,k,h,c,t,b,Ca,Ta,Ia,Tr,G,NX)
+        st.markdown('**MERCADO DE DINERO Y LA RECTA LM**')
+        st.write('Tenemos la forma de la ecuación de demanda de dinero:')
+        st.latex(results1[0])
+        st.write('''El mercado de dinero debe estar en equilibrio, por lo tanto la oferta monetaria 
+                real es igual a la demanda de dinero:''')
+        st.latex(results1[2])
+        st.write('De forma general, se obtiene igualando:')
+        st.latex(results1[4])
+        st.markdown('**DEMANDA AGREGADA Y LA RECTA IS**')
+        st.write('''Sabemos de forma general que la Demanda Agregada de una economia 
+                tiene ciertos componenetes autonomos que se expresan en la sigueinte ecuación:''')
+        st.latex(results1[6])
+        st.write('''Cuando el mercado está en equilibrio, es decir, cuando
+            la demanda agregada DA es igual al nivel de renta Y, se obtiene la recta IS a partir
+                de la siguiente expresión:''')
+        st.latex(results1[8])
+        st.markdown('**EQUILIBRIO IS-LM**')
+        st.write('Se igualan las rectas IS y LM, de forma general:')
+        st.latex(results1[10])
+        st.write('Despejando se obtiene el nivel de renta de equilibrio:')
+        st.latex(results1[12])
+        st.write('Y ahora se pasa el nivel de renta a las rectas IS o LM:')
+        st.latex(results1[14])
+        st.markdown("""---""")
     
-    
-    #M,P,k,h,c,t,b,Ca,Ta,Ia,Tr,G,NX = tuple(user_input_parameters().loc['Parameters'])
-    st.dataframe(user_input_parameters())
-    #st.success(ISLM(M,P,k,h,c,t,b,Ca,Ta,Ia,Tr,G,NX)[0])
 
-    chart_data = pd.DataFrame(
-    np.random.randn(20, 3),
-    columns=['2', 'b', 'c'])
-    st.line_chart(chart_data)
 
+    ##BODY
+    leftcol, rightcol = st.columns([2,1])
+
+    with leftcol:
+        tab1, tab2 = st.tabs(["Parámetros","📈 Gráficos"])
+
+        with st.container():
+            tab1.subheader('Entrada de Parámetros')
+            Mp = tab1.slider('Oferta Monetaria (M)',0,10000,400)
+            kp = tab1.slider('Sensibilidad a la Renta (k)',0,10000,4)
+            Gp = tab1.slider('Gasto (G)',0,10000,500)
+            hp = tab1.slider('Sensibilidad al tipo de interes (h)',0,10000,50)
+            Pp = tab1.slider('Nivel de Precios (P)',0,10000,1)
+            cp = tab1.slider('Propensión al consumo (c)',min_value=float(0), max_value=float(1),value=0.8,step=0.01)
+            tp = tab1.slider('Tasa Impositiva (t)',min_value=float(0), max_value=float(1),value=0.2,step=0.01)
+            bp = tab1.slider('Sensibilidad de la inversion (b)',0,10000,40)
+            Trp = tab1.slider('Transferencias (Tr)',0,10000,100)
+            Cap = tab1.slider('Consumo Autonomo (Ca)',0,10000,180)
+            Tap = tab1.slider('Impuesto Autonomo (Ta)',0,10000,50)
+            Iap = tab1.slider('Interes Autonomo (Ia)',0,10000,50)  
+            NXp = tab1.slider('Exportaciones Netas (NX)',0,10000,50)
+            data = {'Oferta Monetaria':round(Mp,2), 'Nivel de Precios':round(Pp,2), 'Sensibilidad a la Renta':round(kp,2),
+                                    'Sensibilidad al tipo de interes':round(hp,2), 'Pmg':round(cp,2), 'Tasa Impositiva':round(tp,2),
+                                    'Sensibilidad de la inversion':round(bp,2), 'Consumo Autonomo':round(Cap,2),'Impuesto Autonomo':round(Tap,2),
+                                    'Inversión Autonoma':round(Iap,2),'Trasnferencias':round(Trp,2),'Gasto':round(Gp,2),'Exportaciones Netas':round(NXp,2)}
+            feactures = pd.DataFrame(data,index=['Parameters'])
+            Mp,Pp,kp,hp,cp,tp,bp,Cap,Tap,Iap,Trp,Gp,NXp = tuple(feactures.loc['Parameters'])
+            plt, xresults = ISLM(Mp,Pp,kp,hp,cp,tp,bp,Cap,Tap,Iap,Trp,Gp,NXp)
+        tab2.subheader('Grafica de Equilibrio de Mercado')
+        tab2.pyplot(plt)
+        tab2.markdown('''La gráfica muestra el punto de equilibrio/intercepción entre 
+        ambos mercados, es decir de las rectas IS-LM. Dando el nivel de renta de equilibrio y la 
+        tasa de interes de equilibrio''')
+
+    with rightcol:
+        tab1, tab2 = st.tabs(["Modelo","Procedimiento"])
+        tab1.subheader('Parametros Configurados')
+        equations = ISLM_get_description(Mp,Pp,kp,hp,cp,tp,bp,Cap,Tap,Iap,Trp,Gp,NXp)
+        for i in equations:
+            tab1.latex(i)
+
+        tab2.subheader('Procedimiento')
+        tab2.markdown('Puedes Ejecutar el procedimiento, dando click en el botón')
+        if tab2.button('Ejecutar'):
+            results = ISLM_deploy(Mp,Pp,kp,hp,cp,tp,bp,Cap,Tap,Iap,Trp,Gp,NXp)
+            tab2.markdown('**Obtener la recta LM**')
+            tab2.write('La demanda de dinero, como se vio anteriormente, quedaría:')
+            tab2.latex(results[1])
+            tab2.write('Si igualamos la oferta monetaria real con la demanda de liquidez:')
+            tab2.latex(results[3])
+            tab2.latex(results[5])
+            tab2.write('La última expresión representa la recta LM')
+            tab2.markdown('**Obtener la recta IS**')
+            tab2.write('Partiendo de DA=Y, tenemos que el componente autonomo y la recta IS de esta economía son:')
+            tab2.latex(results[7])
+            tab2.latex(results[9])
+            tab2.markdown('**Equilibrio IS-LM**')
+            tab2.write('Al igualar las rectas se obtiene:')
+            tab2.latex(results[11])
+            tab2.write('Si despejamos para Y, se obtiene la renta de equilibrio:')
+            tab2.latex(results[13])
+            tab2.write('Y por lo tanto una tasa de interes de:')
+            tab2.latex(results[15])
+
+    st.markdown("""---""")
+
+   
     
-    x = st.slider('x')  # 👈 this is a widget
-    st.write(x, 'squared is', x * x)
+   
 
 if __name__ == '__main__':
     main()
--- is_lm.py
+++ is_lm.py
@@ -1,20 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+import sympy as sp
+from PIL import Image
 
-
-
-def LM(M,P,k,h):
-  i = np.linspace(0,10,num=100)
-  M_P = M/P 
-  Y = M_P/k + (h*i)/k #RECTA LM
-
-def IS(c,t,r,Ca=0,Ta=0,Ia=0,G=0,Tr=0,NX=0):
-  i = np.linspace(0,10,num=100)
-  A = Ca + Ia + G + NX + c*Tr - c*Tr
-  Y = (A-r*i)/(1-c*(1-t)) # RECTA IS
+def definition():
+    image = Image.open('islm.jpg')
+    return image
 
 def ISLM(M,P,k,h,c,t,b,Ca=0,Ta=0,Ia=0,Tr=0,G=0,NX=0):
-  i = np.linspace(0,10000000)
+  i = np.linspace(0,1000000)
   M_P = M/P 
   Y1 = M_P/k + (h*i)/k
   A = Ca + Ia + G + NX + c*Tr - c*Ta
@@ -22,12 +17,59 @@ def ISLM(M,P,k,h,c,t,b,Ca=0,Ta=0,Ia=0,Tr=0,G=0,NX=0):
   C = np.array([[k,-h],[1-c*(1-t),b]])
   B = np.array([[M_P],[A]])
   X = np.linalg.inv(C).dot(B)
-  P1 = plt.plot(i, Y1)
-  P2 = plt.plot(i, Y2)
-  plt.xlim(0,float(X[1]) + 3)
-  plt.ylim(0,int(X[0]+100))
-  plt.xlabel('Interest Rate')
-  plt.ylabel('Total Output')
-  plt.title('IS-LM Model')  
-  plt.show()
-  return P1,P2,X
+  fig, ax = plt.subplots()
+  ax.plot(i, Y1)
+  ax.plot(i,Y2)
+  ax.set_xlim(0,float(X[1]) + 3)
+  ax.set_ylim(0,int(X[0]+100))
+  ax.set_xlabel('Interest Rate')
+  ax.set_ylabel('Total Output')
+  ax.set_title('IS-LM Model')  
+  return fig, pd.DataFrame(X,index=['Nivel de Renta','Tasa de Interes'],columns=['Equilibrio Economía'])
+
+M,P,k,h,c,t,b,Ca,Ta,Ia,Tr,G,NX,L,Y,i,DA,I,T,C = sp.symbols('M,P,k,h,c,t,b,Ca,Ta,Ia,Tr,G,NX,L,Y,i,DA,I,T,C')
+
+def to_symp(lista,a=0):
+    result = sp.sympify(str(lista[a])) ##convert a solution of a sympify object (intermediate computations)
+    return result
+
+def ISLM_get_description(M,P,k,h,c,t,b,Ca,Ta,Ia,Tr,G,NX):
+    ## get the equations of the model
+    Leq = sp.Eq(L,k*Y-h*i)
+    M_a = sp.Eq(sp.symbols('M_s'),M)
+    Peq = sp.Eq(sp.symbols('P^_'),P)
+    Treq = sp.Eq(sp.symbols('Tr^_'),Tr)
+    Geq = sp.Eq(sp.symbols('G^_'),G)
+    NXeq = sp.Eq(sp.symbols('NX^_'),NX)
+    Teq = sp.Eq(T,Ta + t*Y)
+    Yd = Y - Teq.rhs + Tr
+    Ydeq = sp.Eq(sp.symbols('Y_d'),Yd)
+    Ceq = sp.Eq(C,Ca+c*Yd)
+    Ieq = sp.Eq(I,Ia-b*i)
+    list = [Leq,M_a,Peq,Treq,Geq,NXeq,Teq,Ydeq,Ceq,Ieq]
+    return [sp.latex(i) for i in list]
+
+def ISLM_deploy(M_,P_,k_,h_,c_,t_,b_,Ca_,Ta_,Ia_,Tr_,G_,NX_): 
+    Leq = sp.Eq(L,k*Y-h*i)
+    Leq_ = sp.Eq(L,k_*Y-h_*i)
+    MPeq = sp.Eq(L,M/P)
+    MPeq_ = MPeq.subs(M/P,M_/P_)
+    LM = sp.Eq(i,to_symp(sp.solve(sp.Eq(MPeq.rhs,Leq.rhs),i)))
+    LM_ = sp.Eq(i,to_symp(sp.solve(sp.Eq(Leq_.rhs,MPeq_.rhs),i))).evalf(3)
+    A = sp.Eq(sp.symbols('A'),Ca + Ia + G + NX + c*Tr - c*Ta)
+    LS = sp.Eq(i,(A.lhs-(1-c*(1-t))*Y)/b)
+    A_ = sp.Eq(sp.symbols('A'),
+            A.rhs.subs(Ca,Ca_).subs(Ia,Ia_).subs(NX,NX_).subs(G,G_).subs(c,c_).subs(Tr,Tr_).subs(Ta,Ta_))
+    LS_ = sp.Eq(i,LS.subs(sp.symbols('A'),A_.rhs).subs(c,c_).subs(t,t_).subs(b,b_).rhs).evalf(3)
+    iequ = sp.Eq(LM.rhs,LS.rhs)
+    iequ_ = sp.Eq(LM_.rhs,LS_.rhs)
+    yeq = sp.Eq(Y,to_symp(sp.solve(iequ,Y)))
+    yeq_ = sp.Eq(Y,to_symp(sp.solve(iequ_,Y)))
+    ieq = sp.Eq(i,LS.subs(Y,yeq.rhs).rhs)
+    ieq_ = sp.Eq(i,LM_.subs(Y,yeq_.rhs).rhs.evalf(3))
+    list = [Leq,Leq_,MPeq,MPeq_,LM,LM_,A,A_,LS,LS_,iequ,iequ_,yeq,yeq_,ieq,ieq_]
+    return [sp.latex(i) for i in list]
+
+
+if __name__ == '__main__':
+    ISLM_deploy(M,P,k,h,c,t,b,Ca,Ta,Ia,Tr,G,NX)
